17346182ffa175638eaab46cc17c3229c040f89a
urinir2020 <112821482+urinir2020@users.noreply.github.com>
2022-09-07 12:19:40 +0300

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,11 +1,18 @@
-input.onButtonPressed(Button.A, function () {
-    SuperBit.Servo(SuperBit.enServo.S1, 90)
-})
-input.onButtonPressed(Button.AB, function () {
-    SuperBit.Servo(SuperBit.enServo.S1, 0)
-})
-input.onButtonPressed(Button.B, function () {
-    SuperBit.Servo(SuperBit.enServo.S1, 45)
-})
 basic.showIcon(IconNames.Happy)
-SuperBit.Servo(SuperBit.enServo.S1, 0)
+for (let index = 0; index < 4; index++) {
+    SuperBit.MotorRunDual(
+    SuperBit.enMotors.M1,
+    255,
+    SuperBit.enMotors.M3,
+    255
+    )
+    basic.pause(3000)
+    SuperBit.MotorRunDual(
+    SuperBit.enMotors.M1,
+    255,
+    SuperBit.enMotors.M3,
+    -255
+    )
+    basic.pause(650)
+}
+SuperBit.MotorStopAll()
